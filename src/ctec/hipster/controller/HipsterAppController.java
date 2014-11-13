c9c1625b0790e16a2d9feaba727c7934cf1b88bd
erob456 <erob456@gmail.com>
2014-11-13 13:41:51 -0700

    Added Photo

--- src/ctec/hipster/controller/HipsterAppController.java
+++ src/ctec/hipster/controller/HipsterAppController.java
@@ -1,18 +1,40 @@
 package ctec.hipster.controller;
 
+import ctec.hipster.model.Hipster;
 import ctec.hipster.view.HipsterFrame;
 
 public class HipsterAppController
 {
 	private HipsterFrame baseFrame;
-
+	private Hipster selfHipster;
+	private String [] myAlbums;
+	
+	
 	public HipsterAppController()
 	{
+		selfHipster = new Hipster("Eric", 17);
+		buildAlbumArray();
 		baseFrame = new HipsterFrame(this);
 	}
 
 	public void start()
 	{
-
+		selfHipster.setHipsterAlbums(myAlbums);
+		
+	}
+	
+	private void buildAlbumArray()
+	{
+		String [] myAlbums = new String[4];
+		myAlbums[0] = "Iron and Wine: some hipsterness";
+		myAlbums[1] = "Modest Mouse: used to be hipster";
+		myAlbums[2] = "adlkjasjofsd";
+		myAlbums[3] = "sads asdij eoij flij akljsldrij";
+		
+		String [] otherAlbums = {"stuff", "other", "way for", "hipster array"};
+		
+		String [] weirdAlbums;
+		
+		weirdAlbums = new String [] {"weird", "sad", "sda", "slkaj",};
 	}
 }
--- src/ctec/hipster/view/HipsterPanel.java
+++ src/ctec/hipster/view/HipsterPanel.java
@@ -2,6 +2,7 @@ package ctec.hipster.view;
 
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,20 +23,20 @@ public class HipsterPanel extends JPanel
 	{
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
-		this.setLayout(1000, 300);
 		albumLabel = new JLabel("Album info here");
-		hipsterImage = new JLabel("name");
 		albumBox = new JComboBox();
+		baseLayout.putConstraint(SpringLayout.WEST, albumBox, 101, SpringLayout.EAST, albumLabel);
+		baseLayout.putConstraint(SpringLayout.SOUTH, albumBox, -116, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, albumLabel, 3, SpringLayout.NORTH, albumBox);
+		hipsterImage = new JLabel("Eric Selfie", new ImageIcon(HipsterPanel.class.getResource("/ctec/hipster/view/images/CTECHipsterResize.jpg")), JLabel.CENTER);
+		baseLayout.putConstraint(SpringLayout.EAST, hipsterImage, -697, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, albumLabel, 94, SpringLayout.EAST, hipsterImage);
+		baseLayout.putConstraint(SpringLayout.NORTH, hipsterImage, -74, SpringLayout.NORTH, albumBox);
 		
 		setupComboBox();
 		setupPanel();
 		setupLayout();
 		setupListeners();
-	}
-	
-	private void setLayout(int i, int j)
-	{
-		this.setLayout(1000, 300);
 	}
 
 	private void setupComboBox()
@@ -47,6 +48,7 @@ public class HipsterPanel extends JPanel
 	{
 		this.setBackground(Color.YELLOW);
 		this.setLayout(baseLayout);
+		this.setSize(1000,300);
 		this.add(albumBox);
 		this.add(albumLabel);
 		this.add(hipsterImage);
@@ -54,7 +56,6 @@ public class HipsterPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
 	}
 	
 	private void setupListeners()
